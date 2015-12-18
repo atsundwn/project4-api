4a94f03760008b61d7060d98eeea785334260ab3
Minsup Ko <atsundwn@gmail.com>
2015-12-17 20:54:18 -0500

    Fixes another mistake.

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -26,7 +26,7 @@ class Reminder < ActiveRecord::Base
     hour = send_date.split(' ')[1].split(':')[0].to_i
     minute = send_date.split(' ')[1].split(':')[1].to_i
 
-    hour - 24 if hour >= 19
+    hour -= 24 if hour >= 19
 
     Time.new(year, month, day, hour + 5, minute)
   end
